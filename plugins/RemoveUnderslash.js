da1d871d439ce361a2fab04242065a3b03680a41
Axel Janssen <janssen.axel@gmail.com>
2024-10-03 15:29:23 +0200

    v0.21

---
+++ plugins/RemoveUnderslash.js
@@ -0,0 +1,20 @@
+// RemoveUnderslash.js
+;(function () {
+  class MyPlugin {
+    install() {}
+    configure(pluginManager) {
+      pluginManager.jexl.addFunction('removeus', feature => {
+          const replacedFeature = feature.replace(/_/g, '');
+            return replacedFeature
+//          return replacedFeature.charAt(0).toUpperCase() + replacedFeature.slice(1);
+//        return feature.replace(/_/g, ' ').charAt(0).toUpperCase() + feature.slice(1); 
+      })
+    }
+  }
+
+  // the plugin will be included in both the main thread and web worker, so
+  // install plugin to either window or self (webworker global scope)
+  ;(typeof self !== 'undefined' ? self : window).JBrowsePluginRemoveUnderslash = {
+    default: MyPlugin,
+  }
+})()
